0824199f8d58e1c68413acdb15a9f050343a41f0
gabrielsantos2014626-prog <gabriel.santos2014626@gmail.com>
2026-09-05 23:54:38 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const FinApp());
 }
 
